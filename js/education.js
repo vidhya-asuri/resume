466230dc134f4e5db5e8bbacfeffc664ecaa35a2
vidhya <vid.sigsoln@gmail.com>
2015-11-23 03:36:33 +0000

    change flex direction for skills and top-contacts; add url for educational institutions; fix naming in education javascript object

--- js/education.js
+++ js/education.js
@@ -1,26 +1,45 @@
+/*
+schools: array of objects with
+     name: string
+     location: string
+     degree: string
+     majors: array of strings
+     dates: integer (graduation date)
+     url: string
+onlineCourses: array of objects with
+     title: string
+     school: string
+     date: integer (date finished)
+     url: string
+display: function
+*/
+
 var education =
   {
     "schools" : [
       {
         "name" : "Udacity",
-        "city" : "California, USA",
+        "location" : "California, USA",
         "degree" : "Front End Web developer",
         "major" : "Front end web development",
-        "years" : "Sep 2015 - Jan 2016"
+        "dates" : "Sep 2015 - Jan 2016",
+        "url" : "http://www.udacity.com"
       },
       {
         "name" : "University of Kansas",
-        "city" : "Kansas, USA",
+        "location" : "Kansas, USA",
         "degree" : "Masters",
         "major" : "Elec. Engg.",
-        "years" : "Jan 1998 - Dec 2000"
+        "dates" : "Jan 1998 - Dec 2000",
+        "url" : "http://www.ku.edu"
       },
       {
         "name" : "Annamalai University",
-        "city" : "India",
+        "location" : "India",
         "degree" : "BE",
         "major" : "Elec. Engg.",
-        "years" : "1992-1996"
+        "dates" : "1992-1996",
+        "url" : "http://www.annamalaiuniversity.ac.in"
       }
     ]
  };
@@ -34,16 +53,17 @@ var educationSchools  = education["schools"];
 
 for(var i=0; i < educationSchools.length; i++)
 {
-  var school = HTMLschoolName.replace("%data%",educationSchools[i].name);
-  $(".education-entry").append(school);
+  var school = HTMLschoolName.replace("#",educationSchools[i].url);
+  var mySchool = school.replace("%data%",educationSchools[i].name);
+  $(".education-entry").append(mySchool);
 
   var degree = HTMLschoolDegree.replace("%data%",educationSchools[i].degree);
   $(".education-entry").append(degree);
 
-  var city = HTMLschoolLocation.replace("%data%",educationSchools[i].city);
+  var city  = HTMLschoolLocation.replace("%data%",educationSchools[i].location);
   $(".education-entry").append(city);
 
-  var date = HTMLschoolDates.replace("%data%",educationSchools[i].years);
+  var date = HTMLschoolDates.replace("%data%",educationSchools[i].dates);
   $(".education-entry").append(date);
 
   var major = HTMLschoolMajor.replace("%data%",educationSchools[i].major);
